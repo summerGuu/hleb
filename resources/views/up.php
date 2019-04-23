c4447d79620d15806ab9be6a38ff575716b3fea5
Foma Tuturov <foma@pdg.ru>
2019-04-23 18:48:55 +0300

    + map

---
+++ resources/views/up.php
@@ -0,0 +1,1 @@
+<div style="min-height: 100px; border: 1px solid rgba(253,248,99,0.49)">Top</div>
--- routes/main.php
+++ routes/main.php
@@ -1,7 +1,8 @@
 <?php
-
+Route::renderMap('main', ["up", "content", "bottom"] );
 Route::get("/", view("default"));
 Route::getGroup();
+Route::get("/map/", render("main"));
 Route::get("/{page}/")->controller("PageController")->where(["page" => "^((?!protect).)*"]);
 Route::get("/{page}/ajax/")->controller("PageController@ajax");
 Route::endGroup();
